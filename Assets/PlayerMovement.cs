a2b7471529a93506d53752bf6522473826d3e0e2
iSweetJS <67283947+SweetJS64@users.noreply.github.com>
2022-04-26 18:44:18 +0400

    fire in my ass

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,16 +6,16 @@ public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public CharacterController2D controller;
-
     public float runSpeed = 40f;
     float horizontalMove = 0f;
     bool jump = false;
+   
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-        if (Input.GetButtonUp("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
             jump = true;
         }
